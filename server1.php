26657c2e3ad265bb486f67b57eccaaa8fc44f77a
xrTor <34675070+xrTor@users.noreply.github.com>
2025-09-18 22:39:26 +0300

    Delete server1.php

--- server1.php
+++
@@ -1,6 +0,0 @@
-<?
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    // במקום להדפיס הודעה גנרית, נציג את השגיאה המדויקת מ-MySQL
-    die("Connection failed: " . htmlspecialchars($conn->connect_error));
-}
